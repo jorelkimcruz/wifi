5649a44d6e46f0a65875ceeb0e7e09f7e8855256
Jorel Cruz <jkim@tdjasia.com>
2021-12-07 16:23:48 +0800

    - test migration

--- lib/wifi.dart
+++ lib/wifi.dart
@@ -7,15 +7,15 @@ enum WifiState { error, success, already }
 class Wifi {
   static const MethodChannel _channel = const MethodChannel('plugins.ly.com/wifi');
 
-  static Future<String> get ssid async {
+  static Future<String?> get ssid async {
     return await _channel.invokeMethod('ssid');
   }
 
-  static Future<int> get level async {
+  static Future<int?> get level async {
     return await _channel.invokeMethod('level');
   }
 
-  static Future<String> get ip async {
+  static Future<String?> get ip async {
     return await _channel.invokeMethod('ip');
   }
 
@@ -36,7 +36,7 @@ class Wifi {
       'ssid': ssid,
       'password': password,
     };
-    int state = await _channel.invokeMethod('connection', params);
+    int? state = await _channel.invokeMethod('connection', params);
     switch (state) {
       case 0:
         return WifiState.error;
@@ -51,8 +51,8 @@ class Wifi {
 }
 
 class WifiResult {
-  String ssid;
-  int level;
+  String? ssid;
+  int? level;
 
   WifiResult(this.ssid, this.level);
 }
